57e0d80b575e13fad37104dbd295d0407c5cc21c
Alex Johnson <johnsonav1992@gmail.com>
2022-07-19 09:53:24 -0500

    coding challenge update

--- foundations/week_2/has_unique_chars.js
+++ foundations/week_2/has_unique_chars.js
@@ -23,3 +23,57 @@ function hasUniqueCharsAlt(str) {
 
 console.log(hasUniqueCharsAlt("Monondaydam"))
 
+////////FIND LUCKY NUMBERS//////////
+function findLuckyNumbers(n) {
+	let result = []
+	
+	if(n < 0 || n > 10) return "Please select between 1 & 10!"
+
+	while(result.length < n){
+		let i = 0
+		let newNum = Math.floor((Math.random() * 10) + 1)
+		
+		if(result.includes(newNum)) {
+			result.splice(result[i], 1)
+		} else {
+			result.push(newNum)
+		}
+		
+		i++
+	}
+
+	return result
+		
+
+}
+
+console.log(findLuckyNumbers(9))
+
+const luckyNumbers = n => {
+	let uniqueSet = new Set([])
+	while(uniqueSet.size < n){
+	  uniqueSet.add(Math.floor(Math.random() * 10) + 1)
+	}
+	return [...uniqueSet]
+  }
+  
+  console.log(luckyNumbers(9))
+
+function findLuckyNumbers2(n) {
+    let result = []
+    
+    if(n < 0 || n > 10) return "Please select between 1 & 10!"
+
+    while(result.length < n){
+        let newNum = Math.floor(Math.random() * 10)
+        
+        if(result.indexOf(newNum) <= 0) {
+			result.push(newNum)
+        }
+    }
+
+    return result
+}
+
+console.log(findLuckyNumbers(9))
+
