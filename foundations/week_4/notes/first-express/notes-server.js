342fc34a61ce5007479dd477659a45a514daa279
Alex Johnson <johnsonav1992@gmail.com>
2022-08-01 11:42:12 -0500

    notes commit

---
+++ foundations/week_4/notes/first-express/notes-server.js
@@ -0,0 +1,44 @@
+const express = require('express') //getting our express function
+const cors = require('cors') // getting cors dependency 
+const app = express() //setting app to the invocation of express function
+
+app.use(express.json()) //allows JSON to be accepted
+app.use(cors()) // use cors
+
+// app.get(route, function)
+
+const magicSet = [
+    {id: 1, setName: "KTK"},
+    {id: 2, setName: "2X2"},
+    {id: 3, setName: "RAV"},
+]
+
+app.get('/api/magicSet', (req, res) => {
+    res.send(magicSet)
+})
+
+app.post('/api/magicSet', (req, res) => {
+    magicSet.push(req.body)
+    res.send(req.body)
+})
+
+app.put('/api/magicSet/:set_id', (req, res) => {
+    console.log(req.params.set_id)
+    const set = magicSet.find(setObj => {
+        setObj.id === parseInt(req.params.setId)
+    })
+    set.setName = req.body.setName
+    res.status(200).json({message: "ok"})
+})
+
+app.delete('api/magicSet/:set_id', (req, res) => {
+    const set = magicSet.find(setObj => {
+        setObj.id === parseInt(req.params.setId)
+    })
+    magicSet.splice(req.params.set_id - 1, 1)
+    res.send('you did it')
+})
+
+app.listen(8000, () => {
+    console.log("We are live!")
+}) //puts our server live on a specific port
